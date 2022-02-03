b1615b10aa02c68aded27622edfb9464271c6664
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2022-02-03 21:43:02 +0100

    fix: removed _assertTrue in backsight

--- apps/backsight/src/index.ts
+++ apps/backsight/src/index.ts
@@ -1,10 +1,8 @@
-import { _assertTrue } from 'utils';
 import { createServer } from './server';
 
 const port = process.env.PORT || 4001;
 const server = createServer();
 
 server.listen(port, () => {
-   _assertTrue(port === 4001);
    console.log(`api running on ${port}`);
 });
