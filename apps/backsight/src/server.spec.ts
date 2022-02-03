ffa365a5eba772c9171bcbade38d96a066d3fc31
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2022-02-03 21:46:27 +0100

    fix: server spec is now valid

--- apps/backsight/src/server.spec.ts
+++ apps/backsight/src/server.spec.ts
@@ -2,9 +2,9 @@ import supertest from 'supertest';
 import { createServer } from './server';
 
 describe('server', () => {
-   it('health check returns 200', async () => {
+   it('root returns 200', async () => {
       await supertest(createServer())
-         .get('/healthz')
+         .get('/')
          .expect(200)
          .then((res) => {
             expect(res.body.ok).toBe(true);
